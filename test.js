83a9055b0e027756c67635b918e87716b2222e9d
andrew luetgers <andrew.luetgers@gmail.com>
2015-06-07 18:00:36 -0500

    renaming project

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-var getset = require("./index");
+var dlRange = require("./index");
 
 
 var urlTmpl = "http://lcweb2.loc.gov/service/rbc/rbc0001/2009/2009pre23451/{4}v.jpg";
 
-getset("Natural_Magick-{4}.jpg", urlTmpl, 5, 10, "/SeriesDownloadTest/NaturalMagic", function() {
+dlRange("Natural_Magick-{4}.jpg", urlTmpl, 5, 10, "/SeriesDownloadTest/NaturalMagic", function() {
 	console.log("Test Finished!");
 });
